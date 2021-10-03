c39d650c4fa83c2f43d477d42b7dd0a6ad64469e
Aswathisunil23 <90394317+Aswathisunil23@users.noreply.github.com>
2021-10-03 19:08:40 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,61 +1,49 @@
 class Pet:
-  none = 0
-  speciesList = ['dog', 'cat', 'horse', 'hamster']
+    speciesList = ['dog', 'cat', 'horse', 'hamster']
 
-  def __init__(self, species = none, name =""):
-    if species.lower() not in self.speciesList:
-      raise Exception
-    self.species = species
-    self.name = name
+    def __init__(self, species="NONE", name=""):
+        if species.lower() not in self.speciesList:
+            raise Exception
+        self.species = species
+        self.name = name
 
-  def __str__(self):
-    returnValue = "Species of:" + self.species + "named"
-    if len(name) > 0:
-      returnValue = returnValue + self.name
-    else:
-      returnValue = returnValue + "unnamed"
-    return returnValue
+    def __str__(self):
+        if self.name == "":
+            return "Species of: {} unnamed".format(self.species)
+        return "Species of: {}, named {}".format(self.species, self.name)
 
 class Dog(Pet):
-  def __init__(self, name = "", chases = "Cats"):
-    Pet.__init__(self, "Dog", name)
-    self.chases = chases
+    def __init__(self, name="", chases="Cats"):
+        Pet.__init__(self, "Dog", name)
+        self.chases = chases
 
-  def __str__(self):
-    returnValue = "Species of Dog =, Named"
-    if len(name) > 0:
-      returnValue = returnValue + self.name
-    else:
-      returnValue = returnValue + "unnamed"
-
-    returnValue = returnValue + "Chases" + self.chases
-    return returnValue
+    def __str__(self):
+        if self.name == "":
+            return "Species of Dog, unnamed Chases {}".format(self.chases)
+        return "Species of: Dog, named {} Chases {}".format(self.name, self.chases)
 
 class Cat(Pet):
-  def __init__(self, name = "", hates = "Dogs"):
-    Pet.__init__(self, "Cat", name)
-    self.hates = hates
+    def __init__(self, name="", hates="Dogs"):
+        Pet.__init__(self, "Cat", name)
+        self.hates = hates
 
-  def __str__(self):
-    returnValue = "Species of Cat =, Named"
-    if len(name) > 0:
-      returnValue = returnValue + self.name
-    else:
-      returnValue = returnValue + "unnamed"
-
-    returnValue = returnValue + "Hates" + self.hates
-    return returnValue
+    def __str__(self):
+        if self.name == "":
+            return "Species of Cat, unnamed Hates {}".format(self.hates)
+        return "Species of: Cat, named {} hates {}".format(self.name, self.hates)
 
 def main():
-  dict = {
-    "Dog": [],
-    "Cat": []
-  }
-  for i in range(5):
-    dict["Dog"].append(Dog("Dog " + i))
-  for i in range(3):
-    dict["Cat"].append(Cat("Cat " + i))
-print(dict["Dog"][0].str())
-print(dict["Cat"][1].str())
+    dict = {
+        "Dog": [],
+        "Cat": []
+    }
+    for i in range(5):
+        dict["Dog"].append(Dog("Dog " + str(i)))
+    for i in range(3):
+        dict["Cat"].append(Cat("Cat " + str(i)))
+
+    print(dict["Dog"][0].str())
+    print(dict["Cat"][1].str())
 main()
+
 
